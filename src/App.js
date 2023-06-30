3b6f15d89486d0c55080c8b9c3107e5cfe104960
kidus Tesfaye <kidus.z3rd@gmail.com>
2023-06-30 18:58:14 +0300

    age prector api done

--- src/App.js
+++ src/App.js
@@ -4,29 +4,25 @@ import Axios from "axios";
 import {useState , useEffect} from "react";
 
 function App() {
+ const [givenName, setGivenName]= useState("");
+  const [age, setAge] = useState("--");
+const handleChange = (event) => {
+
+  setGivenName(event.target.value);
+  console.log(givenName);
+}
 
 
-const [fact, setFact] = useState("");
-
-
-useEffect(()=>{
- 
-  generate();
-
-},[])
-
- 
-const generate = () => {
-  Axios.get("https://excuser-three.vercel.app/v1/excuse/").then((res)=>{
-    setFact(res.data[0].excuse);
-    console.log(res)
+const generate= () => {
+  Axios.get("https://api.agify.io/?name="+ givenName).then((res)=>{
+    setAge(res.data)
   })
-} 
-
+}
   return (  
     <div className="App">
-  <button onClick={generate}>Generate</button>
-  <h3>{fact}</h3>   
+ <button onClick={generate}>Pridict Age</button>
+ <input onChange={handleChange} type="text" />
+ <h1>Your age is: {age.name}</h1>
     </div>
   );
 }
