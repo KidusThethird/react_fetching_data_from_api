373357f8e4e6a7f0c18da7ee7f3bc5510bb4a2d7
kidus Tesfaye <kidus.z3rd@gmail.com>
2023-06-30 17:25:28 +0300

    axios fecth example

--- src/App.js
+++ src/App.js
@@ -1,12 +1,38 @@
 
 import './App.css';
+import Axios from "axios";
+import {useState , useEffect} from "react";
 
 function App() {
+
+
+const [fact, setFact] = useState("");
+
+
+useEffect(()=>{
+ 
+    Axios.get("https://excuser-three.vercel.app/v1/excuse/").then((res)=>{
+      setFact(res.data[0].excuse);
+      console.log(res)
+    })
+
+},[])
+
+ 
+const generate = () => {
+  Axios.get("https://excuser-three.vercel.app/v1/excuse/").then((res)=>{
+    setFact(res.data[0].excuse);
+    console.log(res)
+  })
+} 
+
   return (
     <div className="App">
-      Hello
+  <button onClick={generate}>Generate</button>
+  <h3>{fact}</h3>   
     </div>
   );
 }
 
 export default App;
+// https://excuser-three.vercel.app/v1/excuse/
