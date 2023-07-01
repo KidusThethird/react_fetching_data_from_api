d36a828a3413a2b55d74e7ded3755080cc29e535
kidus Tesfaye <kidus.z3rd@gmail.com>
2023-07-01 11:06:22 +0300

    proccessed

--- src/App.js
+++ src/App.js
@@ -4,25 +4,10 @@ import Axios from "axios";
 import {useState , useEffect} from "react";
 
 function App() {
- const [givenName, setGivenName]= useState("");
-  const [age, setAge] = useState("--");
-const handleChange = (event) => {
 
-  setGivenName(event.target.value);
-  console.log(givenName);
-}
-
-
-const generate= () => {
-  Axios.get("https://api.agify.io/?name="+ givenName).then((res)=>{
-    setAge(res.data)
-  })
-}
   return (  
     <div className="App">
- <button onClick={generate}>Pridict Age</button>
- <input onChange={handleChange} type="text" />
- <h1>Your age is: {age.name}</h1>
+ 
     </div>
   );
 }
