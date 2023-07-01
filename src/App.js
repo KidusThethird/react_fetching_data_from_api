ff0ac760c2c1f24b707bd6a76810b237452e31e6
kidus Tesfaye <kidus.z3rd@gmail.com>
2023-07-01 11:29:24 +0300

    fetching excercise done

--- src/App.js
+++ src/App.js
@@ -5,12 +5,30 @@ import {useState , useEffect} from "react";
 
 function App() {
 
+const[fetchedExcuse, setFetchedExcuse] = useState("");
+
+const fetchData=(type)=> {
+  Axios.get(`https://excuser-three.vercel.app/v1/excuse/${type}`).then((res)=>{
+    setFetchedExcuse(res.data[0]);
+  })
+}
+
+
+
   return (  
     <div className="App">
  
+ <h1>{fetchedExcuse.excuse}</h1>
+ {fetchedExcuse == "" ? <h2>Choose Type</h2> : <h2>Catagory: {fetchedExcuse.category}</h2>}
+ 
+ <button onClick={()=>fetchData("party")}>Party</button>
+ <button onClick={()=>fetchData("family")}>Family</button>
+ <button onClick={()=>fetchData("office")}>Office</button>
     </div>
   );
 }
 
 export default App;
 // https://excuser-three.vercel.app/v1/excuse/
+
+
